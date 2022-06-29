5b4a72b3fad7839af7aab6a69ec3bee073e266e4
TheoStracke <theostracke11@gmail.com>
2022-06-29 11:21:11 -0300

    Fluxo de Telas

---
+++ Bionic Soul/Assets/Scripts/ButtonMaster.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonMaster : MonoBehaviour
+{
+    public GameObject pausa, Objects, Menu, Config, Tutorial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void confirmClick()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void voltarMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+    public void opcoes()
+    {
+        Menu.SetActive(false);
+        Config.SetActive(true);
+    }
+    public void voltarMenuOpcoes()
+    {
+        Menu.SetActive(true);
+        Config.SetActive(false);
+    }
+}
