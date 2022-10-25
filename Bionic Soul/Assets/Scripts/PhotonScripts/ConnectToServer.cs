4957581cdbcf1597e9be0a5edde4d8745b2b24a5
TheoStracke <theostracke11@gmail.com>
2022-10-25 17:12:59 -0300

    Comecei o Multiplayer

--- Bionic Soul/Assets/Scripts/PhotonScripts/ConnectToServer.cs
+++ Bionic Soul/Assets/Scripts/PhotonScripts/ConnectToServer.cs
@@ -3,24 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class ConnectToServer : MonoBehaviourPunCallbacks
 {
     public InputField usernameInput;
     public Text buttonText;
+    public GameObject painelLobby;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
     public void OnClickConnect()
     {
         if(usernameInput.text.Length >= 1)
         {
             PhotonNetwork.NickName = usernameInput.text;
-            buttonText.text = "Connecting...";
+            buttonText.text = "Conectando...";
             PhotonNetwork.ConnectUsingSettings();
         }
     }
     public override void OnConnectedToMaster()
     {
-        base.OnConnectedToMaster();
-        SceneManager.LoadScene("Lobby");
+        painelLobby.SetActive(true);
     }
 }
