e132b61d459d54707285edc2e7201427224d1ecd
TheoStracke <theostracke11@gmail.com>
2022-10-31 16:35:58 -0300

    Refazendo tudo do 0 (Multiplayer)

--- Bionic Soul/Assets/Scripts/PhotonScripts/Launcher.cs
+++ Bionic Soul/Assets/Scripts/PhotonScripts/Launcher.cs
@@ -2,17 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
 
-public class Launcher : MonoBehaviour
+
+public class Launcher : MonoBehaviourPunCallbacks
 {
+    InputField NomePlayer;
+    string gamerversion = "1";
+    const string playerNamePrefKey = "PlayerNamer";
     #region MonoBehavior CallBacks
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        
+    }
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        PhotonNetwork.CreateRoom(null, new RoomOptions {MaxPlayers = 2 });
+    }
     void Awake()
     {
         PhotonNetwork.AutomaticallySyncScene = true;
     }
     private void Start()
     {
-        
+        string defaultName = string.Empty;
+        InputField _inputFielf = this.GetComponent<InputField>();
+        if (_inputFielf != null)
+        {
+            if (PlayerPrefs.HasKey(playerNamePrefKey))
+            {
+                defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+                _inputFielf.text = defaultName;
+            }
+        }
+        PhotonNetwork.NickName = defaultName;
+    }
+    public void SetPlayerName(string value)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            return;
+        }
+        PhotonNetwork.NickName = value;
+
+        PlayerPrefs.SetString(playerNamePrefKey, value);
     }
     #endregion
     public void Connnect()
@@ -20,6 +58,11 @@ public class Launcher : MonoBehaviour
         if (PhotonNetwork.IsConnected)
         {
             PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = gamerversion;
         }
     }
 }
