9567fc2b2072cb6fe2cbce9290b9a8685efab9fc
TheoStracke <theostracke11@gmail.com>
2022-11-21 09:34:25 -0300

    macarronda

--- Bionic Soul/Assets/Scripts/PhotonScripts/Multiplayer Charcters/HealthbarMulti.cs
+++ Bionic Soul/Assets/Scripts/PhotonScripts/Multiplayer Charcters/HealthbarMulti.cs
@@ -8,14 +8,23 @@ public class HealthbarMulti : MonoBehaviour
     [SerializeField] private Playerteste playermul;
     [SerializeField] private Image totalHealthBar;
     [SerializeField] private Image currentHealthBar;
+    public Transform target;
+    public float FollowSpeed = 5f;
+    public float yoffset = 3f;
     void Start()
     {
-        totalHealthBar.fillAmount = playermul.currentHealth / 1;
+        totalHealthBar.fillAmount = playermul.currentHealth / 3;
     }
 
     // Update is called once per frame
     void Update()
     {
-        currentHealthBar.fillAmount = playermul.currentHealth / 1;
+        barraseguir();
+        currentHealthBar.fillAmount = playermul.currentHealth / 3;
+    }
+    void barraseguir()
+    {
+        Vector3 newPos = new Vector3(target.position.x, target.position.y, -10f);
+        transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
     }
 }
