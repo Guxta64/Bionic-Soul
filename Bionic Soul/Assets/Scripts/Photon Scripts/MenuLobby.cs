a5a297bed6c86a9ca5076b30cb4f71874399a9bc
TheoStracke <theostracke11@gmail.com>
2022-11-30 11:36:38 -0300

    mais coisa do multiplayer

--- Bionic Soul/Assets/Scripts/Photon Scripts/MenuLobby.cs
+++ Bionic Soul/Assets/Scripts/Photon Scripts/MenuLobby.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuLobby : MonoBehaviour
 {
+    [SerializeField] private Text _ListadeJogadores;
+    [SerializeField] private Button _comecaJogo;
 
 }
